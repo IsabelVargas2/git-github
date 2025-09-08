900589b294b5747c922a3ee2be9d0dfe9d0cf342
IsabelVargas2 <mvargas859@unab.edu.co>
2025-09-07 22:03:28 -0500

    estructuras de datos y bucles

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ Route::get('/', function () {
     echo "************ VARIABLES Y TIPOS DE VARIABLES ************** <br><br>";
     $name = "Isabel Vargas"; // variable de tipo string
 
-    
     echo $name;
     $age = rand(18, 40); // variable de tipo integer
     $height = 1.50; // variable de tipo float o decimal
@@ -36,21 +35,56 @@ Route::get('/', function () {
 
     echo $messege;
     echo"<br>";
-    echo "********************* FUNCIONES ******************* <br><br>";
+    echo "************************ FUNCIONES ********************** <br><br>";
 
     echo printUser(age : $age, name : $name);
 
     printUserWithCallBack(name : $name, age: $age, callable: function(){
         echo "Esta es una función callback, saludos!!! <br>";
     });
+
+    echo "************** LISTA Y MAPAS ***************** <br><br>";
+
+    $lista= ["Isabel", "Vargas", 19, true];
+
+    echo $lista[1];
+
+    $listProduct =[
+        "Camiseta" => [
+            "name" => "Camisa",
+            "price" => 20.5,
+            "stock" => 100,
+            "isActivate" => true
+        ],
+        "Pantalon" => [
+            "name" => "Pantalon",
+            "price" => 25.5,
+            "stock" => 50,
+            "isActivate" => true
+        ]
+    ];
+
+    echo $listProduct["Camiseta"]["stock"];
+
+    echo "<br>************* BUCLES ***************<br><br>";
+
+    for ($i = 0; $i< count ($lista); $i++) {
+        echo $lista[$i] . "<br>";
+    }
+    foreach ($listProduct as $product) {
+        echo $product["name"] . " - " . $product["price"] . "<br>";
+    }
 });
 
 
-function printUser(string $name, int $age){
+
+function printUser(string $name, int $age)
+{
     return "El nombre del usuario es $name y su edad es $age años <br>";
 }
 
-function printUserWithCallBack(string $name, int $age, callable $callable){
+function printUserWithCallBack(string $name, int $age, callable $callable)
+{
     echo "soy $name y tengo $age años <br>";
-    $callable()
+    $callable();
 }
