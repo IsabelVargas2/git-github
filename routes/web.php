21a78b34f4b919610b64466f1314db875bfa2df7
IsabelVargas2 <mvargas859@unab.edu.co>
2025-09-07 21:20:17 -0500

    Estructuras de control

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,26 @@ Route::get('/', function () {
 
     echo"<br>";
     echo "Mi nombre es $name, tengo $age años y mido $height metros <br>";
+
+    echo"<br>";
+    echo "************ ESTRUCTURAS DE CONTROL ************** <br><br>";
+    echo"<br>";
+
+    $messege = "Hola soy $name";
+
+    if ($age >=18) {
+        $messege .= ", Eres mayor de edad <br>";
+    } else if ($age > 50){
+        $messege .= "Eres Adulto mayor <br>";
+    }
+    else {
+        $messege .= ", Eres menor de edad <br>";
+    }
+
+    $messege .= " ".($isLogin? "Ya estas logueado":"No estas logueado")."<br>";
+
+    echo $messege;
+
+
+
 });
