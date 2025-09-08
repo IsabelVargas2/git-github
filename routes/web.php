2e7ab42122252416ed8b6545249bbb345139a1ef
IsabelVargas2 <mvargas859@unab.edu.co>
2025-09-07 21:02:26 -0500

    variables y tipos de variables

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    
+    echo "************ VARIABLES Y TIPOS DE VARIABLES ************** <br><br>";
+    $name = "Isabel Vargas"; // variable de tipo string
+
+    
+    echo $name;
+    $age = rand(18, 40); // variable de tipo integer
+    $height = 1.50; // variable de tipo float o decimal
+
+    $isLogin = true; // variable de tipo boolean
+
+    echo"<br>";
+    echo "Mi nombre es $name, tengo $age años y mido $height metros <br>";
 });
